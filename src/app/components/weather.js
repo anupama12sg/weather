c19b241ac04247933880fffdccb08e0c0590432c
Anupama Singh <anupama12sg@gmail.com>
2024-09-10 00:21:24 +0530

    feat: Added alert function for when city not found due to incorrect city in input field.

--- src/app/components/weather.js
+++ src/app/components/weather.js
@@ -46,6 +46,12 @@ const Weather = () => {
 
       const response = await fetch(url);
       const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
+
       console.log(data);
       const icon = allIcons[data.weather[0].icon] || Sunny;
       setWeatherData({
